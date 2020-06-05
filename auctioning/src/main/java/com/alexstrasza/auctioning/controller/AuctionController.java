c3976de0fac72fc76a6c53bbfdbce8c1c132b34d
Alexstrasza <sjors.iven@gmail.com>
2020-06-05 10:00:42 +0200

    Work on rest controller

--- auctioning/src/main/java/com/alexstrasza/auctioning/controller/AuctionController.java
+++ auctioning/src/main/java/com/alexstrasza/auctioning/controller/AuctionController.java
@@ -1,9 +1,30 @@
 package com.alexstrasza.auctioning.controller;
 
+import com.alexstrasza.auctioning.models.Auction;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("auction/")
 public class AuctionController
 {
+
+
     // Retrieve auctions with filters
+    @PostMapping("retrieveAuctions")
+    public List<Auction> RetrieveAuctions(@RequestBody byte[] data, @RequestHeader String SocketConnection, @RequestHeader String TimeStamp)
+    {
+        return new ArrayList<>();
+    }
     // Create Auction
+    @PostMapping("new")
+    public boolean CreateNewAuction(@RequestBody Auction data)
+    {
+
+        return true;
+    }
     // Canceling an Auction
     // Post Bid
 
--- auctioning/src/main/java/com/alexstrasza/auctioning/models/Auctioning.java
+++ auctioning/src/main/java/com/alexstrasza/auctioning/models/Auctioning.java
@@ -5,7 +5,27 @@ import java.util.List;
 
 public class Auctioning
 {
+    private static Auctioning instance;
+
     public List<Auction> indexedAuctions = new ArrayStack<>();
 
+    private Auctioning()
+    {
 
+    }
+
+    public static Auctioning GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Auctioning();
+        }
+
+        return instance;
+    }
+
+    public void AddAuction(Auction auction)
+    {
+        indexedAuctions.add(auction);
+    }
 }
